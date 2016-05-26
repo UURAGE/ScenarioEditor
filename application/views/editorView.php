c7509eff42315ed21de55a5b343d258dee573654
Jordy van Dortmont <ydroj@live.nl>
2016-05-26 17:23:16 +0200

    Upgraded to jQuery 2.2.4

--- application/views/editorView.php
+++ application/views/editorView.php
@@ -51,7 +51,7 @@
 
   //non language related scripts
   $scripts = array(
-      "js/lib/jquery-1.12.0.min.js",
+      "js/lib/jquery-2.2.4.min.js",
       "js/lib/jquery-ui-1.11.4.min.js",
       "js/lib/jsPlumb-2.0.7.js",
       //"js/lib/multidraggable.js",
