d4cdef74d100d82463fcf703599e0e65812b16e1
Jordy van Dortmont <ydroj@live.nl>
2016-06-22 16:17:12 +0200

    Updated license and index.php

--- error.php
+++
@@ -1,6 +0,0 @@
-<?php
- // redirect to the error controller, with the status code as an argument
- $num = (string) $_SERVER['REDIRECT_STATUS'];
- // change this URL if the application is not in the web root
- header("Location: /index.php/error/index/" . $num);
-?>
