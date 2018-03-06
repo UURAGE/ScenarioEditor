143cfb87193a36e91022e911393cc8806e3522f7
Jordy van Dortmont <jordy@jordyvandortmont.com>
2018-03-06 16:56:03 +0100

    Upgraded i18next libraries

--- editor/js/i18n.js
+++ editor/js/i18n.js
@@ -70,58 +70,78 @@
             withCredentials: false,
 
             // load languages synchronously, otherwise the editor won't work
-            // taken from: https://github.com/i18next/i18next-xhr-backend/blob/master/i18nextXHRBackend.js#L166
-            // and changed 'x.open(data ? 'POST' : 'GET', url, 1);' to 'x.open(data ? 'POST' : 'GET', url, false);'
-            // and added the _typeof function
-            ajax: function(url, options, callback, data, cache)
-            {
+            // taken from: https://github.com/i18next/i18next-xhr-backend/
+            // and changed 'x.open(data ? 'POST' : 'GET', url, 1);' to 'x.open(data ? 'POST' : 'GET', url, 0);'
+            // and added the _typeof and addQueryString functions
+            ajax: function(url, options, callback, data, cache) {
                 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ?
-                function (obj)
-                {
-                    return typeof obj;
-                }
-                :
-                function (obj)
-                {
-                    return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj;
-                };
+                    function (obj) { return typeof obj; } :
+                    function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
-                // Must encode data
-                if (data && (typeof data === 'undefined' ? 'undefined' : _typeof(data)) === 'object')
-                {
-                    var y = '',
-                        e = encodeURIComponent;
-                    for (var m in data)
-                    {
-                        y += '&' + e(m) + '=' + e(data[m]);
-                    }
-                    data = y.slice(1) + (!cache ? '&_t=' + new Date() : '');
-                }
+                var addQueryString = function(url, params) {
+                    if (params && (typeof params === 'undefined' ? 'undefined' : _typeof(params)) === 'object') {
+                        var queryString = '',
+                            e = encodeURIComponent;
 
-                try
-                {
-                    var x = new (XMLHttpRequest || ActiveXObject)('MSXML2.XMLHTTP.3.0');
-                    x.open(data ? 'POST' : 'GET', url, false);
-                    if (!options.crossDomain)
-                    {
-                        x.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+                        // Must encode data
+                        for (var paramName in params) {
+                        queryString += '&' + e(paramName) + '=' + e(params[paramName]);
+                        }
+
+                        if (!queryString) {
+                        return url;
+                        }
+
+                        url = url + (url.indexOf('?') !== -1 ? '&' : '?') + queryString.slice(1);
                     }
-                    x.withCredentials = !!options.withCredentials;
-                    if (data)
-                    {
-                        x.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-                    }
-                    x.onreadystatechange = function ()
-                    {
-                        x.readyState > 3 && callback && callback(x.responseText, x);
+
+                    return url;
                     };
-                    x.send(data);
+
+                if (data && (typeof data === 'undefined' ? 'undefined' : _typeof(data)) === 'object') {
+                  if (!cache) {
+                    data['_t'] = new Date();
+                  }
+                  // URL encoded form data must be in querystring format
+                  data = addQueryString('', data).slice(1);
                 }
-                catch (e)
-                {
-                    window.console && console.log(e);
+
+                if (options.queryStringParams) {
+                  url = addQueryString(url, options.queryStringParams);
                 }
-            }
+
+                try {
+                  var x;
+                  if (XMLHttpRequest) {
+                    x = new XMLHttpRequest();
+                  } else {
+                    x = new ActiveXObject('MSXML2.XMLHTTP.3.0');
+                  }
+                  x.open(data ? 'POST' : 'GET', url, 0);
+                  if (!options.crossDomain) {
+                    x.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+                  }
+                  x.withCredentials = !!options.withCredentials;
+                  if (data) {
+                    x.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+                  }
+                  if (x.overrideMimeType) {
+                    x.overrideMimeType("application/json");
+                  }
+                  var h = options.customHeaders;
+                  if (h) {
+                    for (var i in h) {
+                      x.setRequestHeader(i, h[i]);
+                    }
+                  }
+                  x.onreadystatechange = function () {
+                    x.readyState > 3 && callback && callback(x.responseText, x);
+                  };
+                  x.send(data);
+                } catch (e) {
+                  console && console.log(e);
+                }
+              }
         }
     });
 })();
