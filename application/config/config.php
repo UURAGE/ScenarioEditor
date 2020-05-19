39143721760c227b716e49ca1b2405a4ce0af56c
Marcell van Geest <marcell@marcell.nl>
2020-05-19 21:38:22 +0200

    Switched to secure cookies for HTTPS

--- application/config/config.php
+++ application/config/config.php
@@ -394,7 +394,7 @@ $config['sess_regenerate_destroy'] = FALSE;
 $config['cookie_prefix']	= '';
 $config['cookie_domain']	= '';
 $config['cookie_path']		= '/';
-$config['cookie_secure']	= FALSE;
+$config['cookie_secure']	= !empty($_SERVER['HTTPS']);
 $config['cookie_httponly'] 	= FALSE;
 
 /*
